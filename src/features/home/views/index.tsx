4927bf2fae8f9f1a760158cc46e94c570ea6289c
amtProjects93 <amtprojects93@gmail.com>
2025-04-08 14:20:28 +0330

    test

--- src/features/home/views/index.tsx
+++ src/features/home/views/index.tsx
@@ -4,11 +4,13 @@ import MeetingList from "./MeetingsList";
 import MeetingForm from "./MeetingForm";
 
 export default function index() {
-    return <div className={`${container} flex flex-col gap-5 mt-5`}>
-        <h1 className="w-full text-center text-xl md:text-2xl lg:text-3xl">
-            Meeting Scheduler
-        </h1>
-        <MeetingList />
-        <MeetingForm />
-    </div>;
+  return (
+    <div className={`${container} flex flex-col gap-5 mt-5`}>
+      <h1 className="w-full text-center text-xl md:text-2xl lg:text-3xl">
+        Meeting Scheduler / برنامه ریزی جلسات
+      </h1>
+      <MeetingList />
+      <MeetingForm />
+    </div>
+  );
 }
